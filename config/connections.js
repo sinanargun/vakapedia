8a0c5a2a25b4ff16bc02302ec8ecadd2ba659bf5
alpmusti <mustafaalp43@gmail.com>
2017-03-25 00:47:43 +0300

    Mongodb connection

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+   mongodb: {    
+     adapter: 'sails-mongo',
+     host: 'ds141490.mlab.com',
+     port: 41490,
+     user: 'getir', //optional
+     password: 'getir', //optional
+     database: 'getir' //optional
+   }
 
   /***************************************************************************
   *                                                                          *
