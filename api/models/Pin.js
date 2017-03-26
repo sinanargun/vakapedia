455899ca55e65b312b231b8d03dd916265642d92
sinan <sinanargun48@gmail.com>
2017-03-26 08:51:14 +0300

    Location_x and location_y range were increased

--- api/models/Pin.js
+++ api/models/Pin.js
@@ -29,15 +29,15 @@ module.exports = {
   findSimilarLocation: function(inputs, cb){
 
 
-	var location_x_upper = parseFloat(inputs.location_x) + 1.0;
-	var location_x_lower = parseFloat(inputs.location_x) - 1.0;
+	var location_x_upper = parseFloat(inputs.location_x) + 5.0;
+	var location_x_lower = parseFloat(inputs.location_x) - 5.0;
 
-	var location_y_upper = parseFloat(inputs.location_y) + 1.0;
-	var location_y_lower = parseFloat(inputs.location_y) - 1.0;
+	var location_y_upper = parseFloat(inputs.location_y) + 5.0;
+	var location_y_lower = parseFloat(inputs.location_y) - 5.0;
 
 
-  	Pin.find({ location_x: { $gt: location_x_lower }, location_x : {$lt: location_x_upper},
-  				location_y: { $gt: location_y_lower }, location_y : {$lt: location_y_upper}
+  	Pin.find({ location_x: { $gte: location_x_lower }, location_x : {$lte: location_x_upper},
+  				location_y: { $gte: location_y_lower }, location_y : {$lte: location_y_upper}
   				 })
   	.exec(cb);	
   }
